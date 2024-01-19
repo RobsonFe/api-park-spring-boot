c9b620b430cb2fc8bec44e75b5880fcb16efb17c
RobsonFe <robson_20ferreira@hotmail.com>
2024-01-19 10:08:45 -0300

    configuracao do banco e do locale

---
+++ src/test/java/com/robson/apipark/ApiParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robson.apipark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
