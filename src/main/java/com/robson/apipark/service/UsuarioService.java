2b071d21b81c95444337ee70c86aa7add4b3e51e
RobsonFe <robson_20ferreira@hotmail.com>
2024-01-22 10:54:52 -0300

    buscando dados da api

--- src/main/java/com/robson/apipark/service/UsuarioService.java
+++ src/main/java/com/robson/apipark/service/UsuarioService.java
@@ -3,16 +3,25 @@ package com.robson.apipark.service;
 import com.robson.apipark.entity.Usuario;
 import com.robson.apipark.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
-    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(
+                ()-> new RuntimeException("Usuario não encontrado!")
+        );
     }
 }
--- src/main/java/com/robson/apipark/web/controller/UsuarioController.java
+++ src/main/java/com/robson/apipark/web/controller/UsuarioController.java
@@ -3,25 +3,35 @@ package com.robson.apipark.web.controller;
 import com.robson.apipark.entity.Usuario;
 import com.robson.apipark.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
-    private final UsuarioService usuarioService;
+    @Autowired
+    private UsuarioService usuarioService;
 
     @PostMapping
     public ResponseEntity<Usuario> create(@Valid @RequestBody Usuario usuario) {
         try {
             Usuario user = usuarioService.salvar(usuario);
             return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        try {
+            Usuario user = usuarioService.buscarPorId(id);
+            return ResponseEntity.ok(user);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
